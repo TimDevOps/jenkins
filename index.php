1a158a0fce1901fdab880c2c2a963d20a9c61a14
TimDevOps <dsi@timspirit.fr>
2017-06-16 17:04:50 +0200

    Changement de background
    Changement de background en jaune

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
  <style type="text/css">
 	body{font-family:"Roboto","Helvetica Neue",Helvetica,Arial,sans-serif;font-size:13px;line-height:1.846;
 	
-		:#666666;background-color:<?php $couleur ="grey";echo $couleur;?>}
+		:#666666;background-color:<?php $couleur ="yellow";echo $couleur;?>}
  </style>
   </head>
   <body>
